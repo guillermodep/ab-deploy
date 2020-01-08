f5c5024d64d474940e124eb486a3a01c2c5d2e71
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-08 10:53:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "Hello Developer, This is VERSION 1.0 Version Jenkins webhook <br><br>";
+ echo "Hello Developer, This is VERSION 2.0 Version Jenkins webhook <br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
