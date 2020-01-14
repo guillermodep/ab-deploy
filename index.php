307e191c648c1dd498ef4c220708e7f0f8d2cc12
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-14 11:40:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
  echo "Hello Developer, This is VERSION 1.0 - GREEN <br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'];
  echo "<br><br>";
- echo "<img src='santecgif.gif' />";
+ echo "<img src='santecgif.gif' "width=200 height=200 />";
 ?>
