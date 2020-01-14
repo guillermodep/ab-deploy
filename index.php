97022a25fd44fef58fa357c9ea4ce2842a7acc2e
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-14 12:17:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<?php
-// FETCH IMAGE & WRITE TEXT
-$img = imagecreatefromgif('santecgif.gif');
-$white = imagecolorallocate($img, 255, 255, 255);
-$txt = "Hello Developer, This is VERSION 1.0 - GREEN and My Pod IP is: " .$_SERVER['SERVER_ADDR'];
-$font = ""; 
-imagettftext($img, 24, 0, 5, 24, $white, $font, $txt);
-
-// OUTPUT IMAGE
-header('Content-type: image/gif');
-imagegif($img);
-imagedestroy($img);
-
-// OR SAVE TO A FILE
-// THE LAST PARAMETER IS THE QUALITY FROM 0 to 100
-// imagejpeg($img, "test.jpg", 100);
+<?php 
+ echo "<body style='background-color:green'>";
+ echo "Hello Developer, This is VERSION 1.0 - GREEN and My Pod IP is: " ".$_SERVER['SERVER_ADDR'];
+ echo "<br><br>";
+ echo "<img src='santecgif.gif'";
 ?>
