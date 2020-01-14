8679525f07263aab3897d0284e53558ce6b05ac7
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-14 10:47:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
- <?php 
- echo "Hello Developer, This is VERSION 2.0 Version Jenkins webhook <br><br>";
- echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
- echo "<img src='santecgif.gif' />";
- ?>
+<?php $image = get_field('santecgif.gif'); 
+echo "Hello Developer, This is VERSION 2.0 Version Jenkins webhook <br><br>";
+echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;// fetch the ACF field ?>
+<html>
+    <head>
+        <title>Santander Tecnologia - Deployment Strategies</title>
+        <style>
+            .your-div {
+                background-image: url('<?php echo $image; ?>');
+            }
+        </style>
+    </head>
+    <body>
+        <!-- this div uses the $image URL as a background. -->
+        <div class="your-div">
+            Lorem Ipsum.
+        </div>
+    </body>
+</html>
