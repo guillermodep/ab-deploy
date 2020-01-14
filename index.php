5fe4514d12772d45764495c9b36ee1cf2b83330f
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-14 12:21:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php 
- echo "<body style='background-color:green'>";
+<?php
  echo "Hello Developer, This is VERSION 1.0 - GREEN and My Pod IP is: " .$_SERVER['SERVER_ADDR'];
  echo "<br><br>";
  echo "<img src='santecgif.gif'";
