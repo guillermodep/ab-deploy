6043781ee7a3a1e6dcaafca902a47c78533e575f
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-14 11:36:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+ echo "<body style='background-color:green'>";
  echo "Hello Developer, This is VERSION 1.0 - GREEN <br><br>";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'];
  echo "<br><br>";
