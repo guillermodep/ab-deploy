2ecf6b80ef528bddef377ee21b0ef57897b7b694
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-14 10:58:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
-<?php $image = get_field('santecgif.gif'); 
-echo "Hello Developer, This is VERSION 2.0 Version Jenkins webhook <br><br>";
-echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;// fetch the ACF field ?>
-<html>
-    <head>
-        <title>Santander Tecnologia - Deployment Strategies</title>
-        <style>
-            .your-div {
-                background-image: url('<?php echo $image; ?>');
-            }
-        </style>
-    </head>
-    <body>
-        <!-- this div uses the $image URL as a background. -->
-        <div class="your-div">
-            Lorem Ipsum.
-        </div>
-    </body>
-</html>
+&lt; ?php
+ 
+//Carregar imagem
+$rImg = ImageCreateFromJPEG("santecgif.gif");
+ 
+//Definir cor
+$cor = imagecolorallocate($rImg, 0, 0, 0);
+ 
+//Escrever nome
+imagestring($rImg,5,126,22,urldecode($_GET['nome']),$cor);
+ 
+//Header e output
+header('Content-type: image/jpeg');
+imagejpeg($rImg,NULL,100);
+ 
+?&gt;
