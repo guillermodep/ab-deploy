244667169a97e14fcc22fa77a59defca42ac796b
garrotemar <42655400+garrotemar@users.noreply.github.com>
2020-01-14 11:58:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<?php 
- echo "<body style='background-color:green'>";
- echo "Hello Developer, This is VERSION 1.0 - GREEN <br><br>";
- echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'];
- echo "<br><br>";
- echo "<img src='santecgif.gif' width=200 height=200 />";
-?>
-
-
 <?php
 // FETCH IMAGE & WRITE TEXT
 $img = imagecreatefromjpeg('santecgif.gif');
