c6f15724ac157250b559a457593a824763600100
guillermodep <58941444+guillermodep@users.noreply.github.com>
2020-01-16 15:28:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ echo "<center><h1>Prueba deploy y alta disponibilidad </h1</center><br/><br/> \n
 
 echo "Hi SRE, This is VERSION 1.0 - GREEN SCREEN ------   My Pod IP is : ";
 
+echo "<span style="color: red;" /><center>Fields marked with <strong>&#40; &#42; &#421</strong> are mandatory!</center></span>";
+
 echo file_get_contents('/etc/hostname');
 
  
