b86c987b302c049caab67e5c7aee90af6aeb5cca
guillermodep <58941444+guillermodep@users.noreply.github.com>
2020-01-16 13:10:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "Hello SRE, This is VERSION 1.0 - GREEN SCREEN ------   My Pod IP is : " .$_SERVER['SERVER_ADDR'];
 
-$hostname == file_get_contents('/etc/hostname');
-echo $hostname
+echo file_get_contents('/etc/hostname');
+
  
 ?>
