859350d636a3eac6ba031b4600756653921eac51
guillermodep <58941444+guillermodep@users.noreply.github.com>
2020-01-16 13:03:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
- echo "Hello SRE, This is VERSION 1.0 - GREEN SCREEN ------   My Pod IP is : " file_get_contents("/etc/hostname");
+ echo "Hello SRE, This is VERSION 1.0 - GREEN SCREEN ------   My Pod IP is : " .$_SERVER['SERVER_ADDR'];
+ echo  file_get_contents("/etc/hostname");
 
  
 ?>
