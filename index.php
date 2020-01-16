0b8128112dc21a90518db9df488cff26fe05c874
guillermodep <58941444+guillermodep@users.noreply.github.com>
2020-01-16 15:31:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ echo "<center><h1>Prueba deploy y alta disponibilidad </h1</center><br/><br/> \n
 
 echo "Hi SRE, This is VERSION 1.0 - GREEN SCREEN ------   My Pod IP is : ";
 
+echo "<p> <font color=blue>One line simple string in blue color</font> </p>";
+
 echo file_get_contents('/etc/hostname');
 
  
