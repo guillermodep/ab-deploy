75b7983f4eb38391b5e08260c5d44b29b2c8198a
Guille Dep <58941444+guillermodep@users.noreply.github.com>
2020-07-20 22:27:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo "<center><h1>Prueba deploy en alta disponibilidad y deploy A - B</h1</center><br/><br/> \n";
 
-echo "Hi SRE, This is VERSION 1.0 ------   My Pod IP is : ";
+echo "Hola SRE, This is VERSION 1.0 ------   My Pod IP is : ";
 
 echo "<p> <font color=red>";
 
